ba7eb64d1d77fc1630a54c5237d7cf090f1c1513
ammarghanem <ammar.ghanem@mbc.net>
2018-04-16 18:57:16 +0400

    6454564

--- js/app.js
+++ js/app.js
@@ -99,8 +99,10 @@ function escapeToClose(event){
      
     }
     //stiky video
+    if(articleVideo){
     var videoPlace = articleVideo.getBoundingClientRect().top;
     var startSticky=  Math.floor( videoPlace +  articleVideo.offsetHeight + window.pageYOffset );
+    }
     window.addEventListener('scroll', function() {
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {} else {
             var header = document.querySelector('header');
