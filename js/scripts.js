d489b6689f5d07dc6d9211491e5c689853eac83d
ammar.ghanem <ammar.ghanem@alarabiya.net>
2019-11-05 18:28:47 +0400

    fsed

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,49 @@
 (function() {
-    'use strict';
+    var second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-    var slider = (document.querySelector('.slider') ? document.querySelector('.slider') : null);
-    if (slider) {
-        var li = slider.querySelectorAll('ul li');
-        var leftBtn = slider.querySelector('button.left');
-        var rightBtn = slider.querySelector('button.right');
-        var score = 0;
+    var countDown = new Date('Oct 20, 2019 00:00:00').getTime(),
+        x = setInterval(function() {
 
-        slider.firstElementChild.style.width = li.length * 900 + 50 + 'px';
+            var now = new Date().getTime(),
+                distance = countDown - now;
 
-        function scroll() {
+            document.getElementById('days').innerText = Math.floor(distance / (day)),
+            document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-            if (this.classList[1] == 'right') {
-                if (li[score].nextElementSibling.nextElementSibling.nextElementSibling == null) return;
-                li[score].style.display = 'none';
-                score++;
-            };
+            //do something later when date is reached
+            //if (distance < 0) {
+            //  clearInterval(x);
+            //  'Hello!;
+            //}
 
-            if (this.classList[1] == 'left') {
-                if (score == 0) return;
-                li[score - 1].style.display = 'inline-block';
-                score--;
-            };
-
-        };
-        rightBtn.addEventListener('click', scroll);
-        leftBtn.addEventListener('click', scroll);
-    }
-    document.querySelector('.menu-icon').addEventListener('click', function(e) {
-
-        document.querySelector('.overlay').classList.toggle('show');
-
-    });
-    var ss = document.querySelector('.overlay');
-
-    ss.addEventListener('click', function(e) {
-        document.querySelector('.menu-btn').checked = false;
-        document.querySelector('.overlay').classList.toggle('show');
-    });
-
-
-    var sections = document.querySelectorAll('section[id]');
-    var navLinks = document.querySelectorAll('nav.about ul li a');
-
-    function getClosestSection() {
-        var sectionsLength = sections.length;
-
-        for (var index = 0; index < sectionsLength; index++) {
-            if (isBelowScroll(sections.item(index)))
-                break;
-        }
-
-        selectLink(sections.item(index).id)
-    }
-
-    function isBelowScroll(element) {
-        var position = element.getBoundingClientRect();
-        return position.top > 0;
-    }
-
-    function selectLink(id) {
-
-        Array.prototype.forEach.call(navLinks, function(element) {
-            element.classList.remove('is-selected');
-        });
-
-        document.querySelector('a[href="#' + id + '"]').classList.add('is-selected');
-    }
-
-    window.addEventListener('scroll', function(event) {
-        getClosestSection();
-    });
-
-
+        }, second)
 })();
+
+document.querySelector('.font-resize').addEventListener('click', function(e) {
+    [].map.call(document.querySelectorAll('article'), function(el) {
+        el.classList.toggle('bigger');
+    });
+});
+window.onload = function() {
+    // Variables
+    var share = document.querySelector('.teaser-tools');
+    var shareTop = share.offsetTop;
+
+    // Functions
+    // You can use toggle instead of add/remove
+    function shareFixed(e) {
+        if (window.scrollY >= shareTop) {
+            share.classList.add('is-fixed');
+        } else {
+            share.classList.remove('is-fixed');
+        }
+    }
+    // Event Listener
+    window.addEventListener('scroll', shareFixed);
+
+}
